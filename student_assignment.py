f7daf58ad928b7bf52341e8706491d6bb6f281a6
hsiuting_yang <hsiuting_yang@asus.com>
2025-02-04 13:53:29 +0800

    RAG2 HW1 test7

--- student_assignment.py
+++ student_assignment.py
@@ -3,9 +3,6 @@ from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
 
-class Chunk:
-    def __init__(self, chunks):
-        self.chunks = chunks
 
 #q1_pdf = "OpenSourceLicenses.pdf"
 #q2_pdf = "勞動基準法.pdf"
@@ -21,21 +18,10 @@ def hw02_1(q1_pdf):
     # Initialize the CharacterTextSplitter
     splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 
-    # Access the text of each page and split it into chunks
-    for i, document in enumerate(pyPDFDocument):
-        pageText = document.page_content
-        chunks = splitter.split_text(pageText)
-        chunks = f"{q1_pdf}, {len(pyPDFDocument)}, {chunks}"
-        pageChunk = Chunk(chunks)
-        #print(f'Text chunks on page {i + 1}:')
-        
-        #for chunk in chunks:
-        #    print(chunk)
-        #    print('-' * 20)
+    chunks = splitter.split_documents(pyPDFDocument)
+    lastChunk = chunks[-1]
 
-    
-
-    return pageChunk
+    return lastChunk
 
 def hw02_2(q2_pdf):
     pass

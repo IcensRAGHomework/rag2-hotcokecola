5e3a8971603bdb2db3973ea2824ca07f2ecc8660
hsiuting_yang <hsiuting_yang@asus.com>
2025-02-04 11:31:37 +0800

    RAG2 HW1 test1

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,25 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    
+    pyPDFLoader = PyPDFLoader(q1_pdf)
+    pyPDFDocument = pyPDFLoader.load()
+    #pyPDFDocumentPages = len(pyPDFDocument)
+    #print(f'The PDF has {len(pyPDFDocument)} pages.')
+
+    # Initialize the CharacterTextSplitter
+    splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+
+    # Access the text of each page and split it into chunks
+    for i, document in enumerate(pyPDFDocument):
+        pageText = document.page_content
+        chunks = splitter.split_text(pageText)
+        #print(f'Text chunks on page {i + 1}:')
+        #for chunk in chunks:
+        #    print(chunk)
+        #    print('-' * 20)
+ 
+    return chunks
 
 def hw02_2(q2_pdf):
     pass

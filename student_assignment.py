796e3373e41905c6d505cbac7551667259436bdf
hsiuting_yang <hsiuting_yang@asus.com>
2025-02-04 11:40:06 +0800

    RAG2 HW1 test2

--- student_assignment.py
+++ student_assignment.py
@@ -2,8 +2,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
-q1_pdf = "OpenSourceLicenses.pdf"
-q2_pdf = "勞動基準法.pdf"
+#q1_pdf = "OpenSourceLicenses.pdf"
+#q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
@@ -11,7 +11,7 @@ def hw02_1(q1_pdf):
     pyPDFLoader = PyPDFLoader(q1_pdf)
     pyPDFDocument = pyPDFLoader.load()
     #pyPDFDocumentPages = len(pyPDFDocument)
-    #print(f'The PDF has {len(pyPDFDocument)} pages.')
+    print(f'The PDF has {len(pyPDFDocument)} pages.')
 
     # Initialize the CharacterTextSplitter
     splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
@@ -20,10 +20,10 @@ def hw02_1(q1_pdf):
     for i, document in enumerate(pyPDFDocument):
         pageText = document.page_content
         chunks = splitter.split_text(pageText)
-        #print(f'Text chunks on page {i + 1}:')
-        #for chunk in chunks:
-        #    print(chunk)
-        #    print('-' * 20)
+        print(f'Text chunks on page {i + 1}:')
+        for chunk in chunks:
+            print(chunk)
+            print('-' * 20)
  
     return chunks
 

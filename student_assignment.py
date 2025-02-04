a5260984bbbe8450988d5d5a118389d51107e6bf
hsiuting_yang <hsiuting_yang@asus.com>
2025-02-04 12:10:42 +0800

    RAG2 HW1 test4

--- student_assignment.py
+++ student_assignment.py
@@ -6,15 +6,6 @@ from langchain_text_splitters import (CharacterTextSplitter,
 #q2_pdf = "勞動基準法.pdf"
 
 
-class Chunk:
-    def __init__(self, pdf_name, page_number, chunk):
-        self.pdfName = pdf_name
-        self.pageNumber = page_number
-        self.chunk = chunk
-
-    def __repr__(self):
-        return f"PDF Name: {self.pdf_name}, Page Number: {self.page_number}, Chunk: {self.chunk}"
-
 def hw02_1(q1_pdf):
     
     pyPDFLoader = PyPDFLoader(q1_pdf)
@@ -26,20 +17,18 @@ def hw02_1(q1_pdf):
     splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 
     # Access the text of each page and split it into chunks
-    chunks = []
     for i, document in enumerate(pyPDFDocument):
         pageText = document.page_content
-        splitChunks = splitter.split_text(pageText)
-        pageChunk = Chunk(q1_pdf, i + 1, splitChunks)
-        chunks.append(pageChunk)
+        chunks = splitter.split_text(pageText)
+        #print(f'Text chunks on page {i + 1}:')
+        
+        #for chunk in chunks:
+        #    print(chunk)
+        #    print('-' * 20)
 
-    # Print the chunks
-    #for chunk in chunks:
-    #    print(chunk)
-    #    print('-' * 20)
-    
-    return pageChunk
-    
+    chunks = f"{chunks}, {len(pyPDFDocument)}!"
+
+    return chunks
 
 def hw02_2(q2_pdf):
     pass

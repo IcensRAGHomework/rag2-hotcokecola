1fd3c11ac80ab90cb9cb87c97dcb61ddc645664c
hsiuting_yang <hsiuting_yang@asus.com>
2025-02-05 18:42:19 +0800

    RAG2 HW2 test2

--- student_assignment.py
+++ student_assignment.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
-
+import re
 #q1_pdf = "OpenSourceLicenses.pdf"
 #q2_pdf = "勞動基準法.pdf"
 
@@ -24,34 +24,39 @@ def hw02_2(q2_pdf):
 
     loader = PyPDFLoader(q2_pdf)
     pyPDFDocument = loader.load()
-
+    
     splitter = RecursiveCharacterTextSplitter(
-        separators=["第.*?章", "第.*?條\n"],
+        separators=[r"第[ ]+.*?[ ]*章", r"第[ ]+.*?[ ]*條"],
         #separators=["\n\n"],
         chunk_size=10,
         chunk_overlap=0,
         is_separator_regex=True
     )
-
+    
     chunks = splitter.split_documents(pyPDFDocument)
     numChunks = len(chunks)
-    #print(numChunks)
-
-    merged_chunks = []
-    buffer = ""
-    for chunk in chunks:
-        text = chunk.page_content  # Access the text content of the chunk
-        #print(text)
-        if not text.startswith("第"):
-            numChunks -= 1
     
+    count=0
+    for i, chunk in enumerate(chunks):
+        text = chunk.page_content  # Access the text content of the chunk
+        #print(f"Chunk {count}:")
+        #print(text)
+        count+=1
+        if i == 0:
+           continue
+        if not text.startswith("第 "):
+           numChunks -= 1
+           count-=1
 
+ 
     #for i, chunk in enumerate(merged_chunks):
     #    print(f"Chunk {i+1}:")
     #    print(chunk)
     #    print("\n" + "-"*50 + "\n")
     
 
-    print(numChunks) 
-
+    #print(numChunks) 
+    
     return numChunks
+
+
